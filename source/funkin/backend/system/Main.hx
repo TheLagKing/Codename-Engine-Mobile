a81d52ab128d63977094f9d0625268cc98dc67eb
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-04-09 09:59:00 +0000

    i think now it'll be cooler, idk?

--- source/funkin/backend/system/Main.hx
+++ source/funkin/backend/system/Main.hx
@@ -66,7 +66,6 @@ class Main extends Sprite
 	    #if android
 		Sys.setCwd(haxe.io.Path.addTrailingSlash(MobileUtil.getDirectory()));
 		MobileUtil.getPermissions();
-		//MobileCrashHandler.checkUncaughtError(); // Gonna see if it'll show error now.
 		#elseif ios
 		Sys.setCwd(lime.system.System.applicationStorageDirectory);
         #end
