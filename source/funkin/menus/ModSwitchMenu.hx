c7fd6b42f7557c267f40cc65dce8af8d2b87fa93
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-04-16 07:34:05 +0000

    stoopod

--- source/funkin/menus/ModSwitchMenu.hx
+++ source/funkin/menus/ModSwitchMenu.hx
@@ -59,6 +59,7 @@ class ModSwitchMenu extends MusicBeatSubstate {
 	  removeVPad();
 	  addVPad(UP_DOWN, A_B_C);
 	  addVPadCamera();
+	  #end
 	 }
 
 	public function changeSelection(change:Int, force:Bool = false) {
