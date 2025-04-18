c6a4b445116317cae84d1a7f0d2297ce182228dd
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-04-18 18:37:15 +0300

    uhhh?

--- commandline/commands/Update.hx
+++ commandline/commands/Update.hx
@@ -14,7 +14,11 @@ class Update {
 		if (!FileSystem.exists('.haxelib'))
 			FileSystem.createDirectory('.haxelib');
 
-		var filename = #if desktop "./libs.xml" #else "./libs-mobile.xml" #end;
+		#if !mobile
+		var filename = "./libs.xml";
+		#else
+		var filename = "./libs-mobile.xml";
+		#end
 		var isSilent = false;
 		for(arg in args) {
 			if (arg.startsWith("--lib=")) {
