897553491a924034640bdca0378204bc71a488c8
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-04-25 18:05:21 +0300

    update source/funkin/options/categories/MiscOptions.hx

--- source/funkin/options/categories/MiscOptions.hx
+++ source/funkin/options/categories/MiscOptions.hx
@@ -29,8 +29,8 @@ class MiscOptions extends OptionsScreen {
 				// TODO: SAVE DATA RESETTING
 		}));
 		#if mobile
-		MusicBeatState.instance.addVPad(UP_DOWN, A_B);
-		MusicBeatState.instance.addVPadCamera();
+		MusicBeatState.getState().addVPad(UP_DOWN, A_B);
+		MusicBeatState.getState().addVPadCamera();
 		#end
 	}
 }
