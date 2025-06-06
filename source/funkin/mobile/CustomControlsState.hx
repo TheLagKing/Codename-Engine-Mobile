bd3631c8a00856db3c74065d734b2bf79dc734f7
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-06-06 08:56:42 +0300

    Update CustomControlsState.hx

--- source/funkin/mobile/CustomControlsState.hx
+++ source/funkin/mobile/CustomControlsState.hx
@@ -13,7 +13,7 @@ import flixel.graphics.frames.FlxAtlasFrames;
 
 using StringTools;
 
-class CustomControlsState extends MusicBeatSubstate {
+class CustomControlsState extends OptionsScreen extends MusicBeatSubstate {
     var _pad:FlxVirtualPad;
     var _hb:HitBox;
 
