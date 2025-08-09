b7d685a8e3e703f519e01b16f323a9b03d57a13c
TheLagKing <dalucanogheaven12@gmail.com>
2025-08-09 22:41:09 +0800

    Update MobileOptions.hx

--- source/funkin/options/categories/MobileOptions.hx
+++ source/funkin/options/categories/MobileOptions.hx
@@ -1,1 +1,14 @@
+package funkin.options.categories;
 
+class AppearanceOptions extends OptionsScreen {
+	public override function new() {
+		super("Mobile Options", "Change Mobile Options. Only on mobile...");
+		add(new Checkbox(
+			"Hitbox Visibility",
+			"Makes the hitbox invisible",
+  }
+    #if mobile
+	 MusicBeatState.instance.addVPad(FULL, A_B);
+	 MusicBeatState.instance.addVPadCamera();
+	 #end
+	}
