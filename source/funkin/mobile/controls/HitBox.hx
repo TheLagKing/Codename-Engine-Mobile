35f95de8161523f15ba861f452d8fea4953c07b1
TheLagKing <dalucanogheaven12@gmail.com>
2025-08-12 16:25:53 +0800

    Add files via upload

--- source/funkin/mobile/controls/HitBox.hx
+++ source/funkin/mobile/controls/HitBox.hx
@@ -3,7 +3,6 @@ package funkin.mobile.controls;
 import flixel.FlxG;
 import flixel.group.FlxSpriteGroup;
 import flixel.util.FlxColor;
-import flixel.ui.FlxButton;
 
 class HitBox extends FlxSpriteGroup
 {
@@ -11,12 +10,12 @@ class HitBox extends FlxSpriteGroup
     public var buttonDown:FlxButton;
     public var buttonUp:FlxButton;
     public var buttonRight:FlxButton;
-    public var buttonSpace:FlxButton;
+    public var buttonSpace:FlxButton = null; // Initialize as null
 
     public function new()
     {
         super();
-        buttonLeft = buttonDown = buttonUp = buttonRight = buttonSpace = new FlxButton(0, 0);
+        buttonLeft = buttonDown = buttonUp = buttonRight = new FlxButton(0, 0);
         
         addButtons();
         scrollFactor.set();
@@ -25,7 +24,7 @@ class HitBox extends FlxSpriteGroup
     function addButtons() {
         var hasExtraButton:Bool = Options.extrabutton == 1;
         var buttonHeight:Int = hasExtraButton ? Std.int(FlxG.height * 0.75) : FlxG.height;
-        var offsetFir:Int = hasExtraButton ? Std.int(FlxG.height * 0.75) : 0;
+        var spaceY:Int = hasExtraButton ? Std.int(FlxG.height * 0.75) : 0;
 
         add(buttonLeft = createHitbox(0, 0, Std.int(FlxG.width / 4), buttonHeight, '0xC24B99'));
         add(buttonDown = createHitbox(FlxG.width / 4, 0, Std.int(FlxG.width / 4), buttonHeight, '0x00FFFF'));
@@ -33,18 +32,19 @@ class HitBox extends FlxSpriteGroup
         add(buttonRight = createHitbox((FlxG.width / 2) + (FlxG.width / 4), 0, Std.int(FlxG.width / 4), buttonHeight, '0xF9393F'));
 
         if (hasExtraButton) {
-            add(buttonSpace = createHitbox(0, offsetFir, FlxG.width, Std.int(FlxG.height * 0.25), '0x7700FF', true));
+            buttonSpace = createHitbox(0, spaceY, FlxG.width, Std.int(FlxG.height * 0.25), '0x7700FF', true);
+            add(buttonSpace);
         }
     }
 
-    function createHitbox(x:Float, y:Float, width:Int, height:Int, color:String, ?isSpace:Bool = false)
+    function createHitbox(x:Float, y:Float, width:Int, height:Int, color:String, ?isSpace:Bool = false):FlxButton
     {
-        var button:FlxButton = new FlxButton(x, y);
+        var button = new FlxButton(x, y);
         button.makeGraphic(width, height, FlxColor.fromString(color));
         button.alpha = Options.hitboxvisibility ? (isSpace ? 0.2 : 0.1) : 0;
 
         button.onDown.callback = () -> {
-            if (Options.hitboxvisibility) button.alpha = isSpace ? 0.2 : 0.1;
+            if (Options.hitboxvisibility) button.alpha = isSpace ? 0.25 : 0.15;
         };
         button.onUp.callback = () -> {
             if (Options.hitboxvisibility) button.alpha = isSpace ? 0.2 : 0.1;
