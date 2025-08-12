afb1cc88e7e420ac31f13513012f7a68f1e059ef
TheLagKing <dalucanogheaven12@gmail.com>
2025-08-12 13:16:30 +0800

    Update MobileOptions.hx

--- source/funkin/options/categories/MobileOptions.hx
+++ source/funkin/options/categories/MobileOptions.hx
@@ -4,12 +4,15 @@ class MobileOptions extends OptionsScreen {
 	public override function new() {
 		super("Mobile Options", "Change Mobile Options. Only on mobile...");
 		add(new Checkbox(
-			"hitbox visibility",
+			"Hitbox Visibility",
 			"Makes the hitbox invisible",
 			"hitboxvisibility"));
-		add(new Checkbox(
-			"extra button",
+		add(new NumOption(
+			"Extra Button",
 			"Adds space button on hitbox",
+			1,
+			2,
+			0,
 			"extrabutton"));
     #if mobile
 	 MusicBeatState.instance.addVPad(FULL, A_B);
