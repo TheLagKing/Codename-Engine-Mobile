69ff2eec44629bcd8e978488a4abe033350df636
TheLagKing <dalucanogheaven12@gmail.com>
2025-08-12 13:12:20 +0800

    Update Options.hx

--- source/funkin/options/Options.hx
+++ source/funkin/options/Options.hx
@@ -19,6 +19,7 @@ class Options
 	public static var naughtyness:Bool = true;
 	public static var downscroll:Bool = false;
 	public static var hitboxvisibility:Bool = true;
+	public static var extrabutton:Bool = false;
 	public static var ghostTapping:Bool = true;
 	public static var flashingMenu:Bool = true;
 	public static var camZoomOnBeat:Bool = true;
