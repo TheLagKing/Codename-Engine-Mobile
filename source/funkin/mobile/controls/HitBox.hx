c281387f9a89dc5f6b870c0526caa926e034fec7
TheLagKing <dalucanogheaven12@gmail.com>
2025-08-17 00:03:12 +0800

    Add files via upload

--- source/funkin/mobile/controls/HitBox.hx
+++ source/funkin/mobile/controls/HitBox.hx
@@ -1,15 +1,13 @@
 package funkin.mobile.controls;
-// start diggin in yo 
+
 import flixel.FlxG;
 import flixel.group.FlxSpriteGroup;
 import flixel.util.FlxColor;
-import flixel.math.FlxMath;
-import openfl.display.Shape;
-import openfl.display.BitmapData;
-import openfl.geom.Matrix;
-/*
-* @author Idklool
-*/
+
+/**
+ * ...
+ * @author Idklool
+ */
 class HitBox extends FlxSpriteGroup
 {
     public var buttonLeft:FlxButton;
@@ -39,39 +37,11 @@ class HitBox extends FlxSpriteGroup
     function createHitbox(x:Float, y:Float, width:Int, height:Int, color:String)
     {
         var button:FlxButton = new FlxButton(x, y);
-        
-        var gradientHeight:Int = Std.int(height * 0.1); // Height of gradient area
-        var baseColor:FlxColor = FlxColor.fromString(color);
-        
-        var shape = new Shape();
-        
-        shape.graphics.beginFill(baseColor, 0.1);
-        shape.graphics.drawRect(0, 0, width, height);
-        shape.graphics.endFill();
-        
-        var matrix = new Matrix();
-        matrix.createGradientBox(width, gradientHeight, Math.PI/2, 0, 0);
-        shape.graphics.beginGradientFill(LINEAR, [baseColor, baseColor], [0.3, 0], [0, 255], matrix);
-        shape.graphics.drawRect(0, 0, width, gradientHeight);
-        shape.graphics.endFill();
-        
-        matrix.createGradientBox(width, gradientHeight, Math.PI/2, 0, height - gradientHeight);
-        shape.graphics.beginGradientFill(LINEAR, [baseColor, baseColor], [0, 0.3], [0, 255], matrix);
-        shape.graphics.drawRect(0, height - gradientHeight, width, gradientHeight);
-        shape.graphics.endFill();
-        
-        var graphic = new BitmapData(width, height, true, 0);
-        graphic.draw(shape);
-        button.loadGraphic(graphic);
-        
-        button.onDown.callback = () -> {
-            button.alpha = 0.15;
-        };
-        
-        button.onUp.callback = () -> {
-            button.alpha = 0.1;
-        };
-        
+        button.makeGraphic(width, height, FlxColor.fromString(color));
+        button.alpha = 0.1;
+
+        button.onDown.callback = () -> button.alpha = 0.15;
+        button.onUp.callback = () -> button.alpha = 0.1;
         button.onOut.callback = button.onUp.callback;
 
         return button;
