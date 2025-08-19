090c0947a2459d8bc8fa0c0ec561d2451cc13037
TheLagKing <dalucanogheaven12@gmail.com>
2025-08-19 21:40:47 +0800

    Fixing grammar 😢

--- source/funkin/options/categories/MobileOptions.hx
+++ source/funkin/options/categories/MobileOptions.hx
@@ -5,25 +5,25 @@ class MobileOptions extends OptionsScreen {
 	var extrabutton:NumOption;
 	
 	public override function new() {
-		super("Mobile Options", "Change Mobile Options. Only on mobile...");
+		super("Mobile Options", "Change Mobile Options...");
 		add(new Checkbox(
 			"Hitbox Visibility",
-			"Makes the hitbox invisible",
+			"Turning this off makes the hitbox invisible.\n Be careful with this.",
 			"hitboxvisibility"));
 		add(new Checkbox(
 			"Classic Buttons",
-			"Bring back the classic buttons",
+			"Bring back the classic buttons.",
 			"classicbuttons"));
 		add(new NumOption(
 			"Extra Button",
-			"Adds space button on hitbox",
+			"Adds extra buttons for hitbox.",
 			0,
 			2,
 			1,
 			"extrabutton"));
 		add(new Checkbox(
 			"Extras Buttons Top",
-			"Makes extra buttons stay at the top of the screen\n(off makes the extra buttons stay below the screen)",
+			"Makes extra buttons stay at the top of the screen.\n(when off, the extra buttons stay at the bottom of the screen)",
 			"extrabuttontop"));
     #if mobile
 	 MusicBeatState.instance.addVPad(FULL, A_B);
